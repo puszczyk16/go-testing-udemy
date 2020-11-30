e177ff5a78d70337d32c8baf7e57cf14f34ac84e
Paweł Żmiejko <pzmiejko@gmail.com>
2020-11-30 16:52:24 +0100

    Added benchmarks for sort service

--- src/api/services/sort_service_test.go
+++ src/api/services/sort_service_test.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"testing"
+	/* in thecourse this line blew didn't needed! Witout it service tests don't work! */
+	"github.com/puszczyk16/go-testing-udemy/src/api/utils/sort"
 )
 
 func TestSort(t *testing.T){
@@ -21,20 +23,21 @@ func TestSortMoraThan10000(t *testing.T) {
 	if elements[0] != 0 {
 		t.Error("first element should be 0")
 	}
-	if elements[len(elements)-1] != 9 {
-		t.Error("first element should be 9")
+	if elements[len(elements)-1] != 10000 {
+		t.Error("first element should be 10000")
 	}
 }
 
-
-/*
-func getElements(n int) []int{
-	result := make([]int, n)
-	j := 0
-	for i := n-1; i > 0; i-- {
-		result[j] = i
-		j++ 
+func BenchmarkBubbleSort10k(b *testing.B) {
+	elements := sort.GetElements(10000)
+	for i := 0; i < b.N; i++ {
+		sort.BubleSort(elements)
 	}
-	return result
 }
-*/
+
+func BenchmarkBubbleSort20k(b *testing.B) {
+	elements := sort.GetElements(20000)
+	for i := 0; i < b.N; i++ {
+		sort.Sort(elements)
+	}
+}
