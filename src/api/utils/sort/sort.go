ebc73265dc37bd9876b7c93c1f1a5671b3544e31
Paweł Żmiejko <pzmiejko@gmail.com>
2020-11-30 11:50:28 +0100

    Added int sort

--- src/api/utils/sort/sort.go
+++ src/api/utils/sort/sort.go
@@ -1,15 +1,23 @@
 package sort
 
+import (
+	"sort"
+)
+
 // BubleSort function
 func BubleSort(elements []int) {
 	keepWorking := true
 	for keepWorking {
 		keepWorking = false
-		for i:=0; i > len(elements)-1; i++ {
+		for i:=0; i < len(elements)-1; i++ {
 			if elements[i] > elements[i+1] {
 				elements[i], elements[i+1] = elements[i+1], elements[i]
 				keepWorking = true
 			}
 		}
 	}
+}
+// Sort function
+func Sort(elements []int) {
+	sort.Ints(elements)
 }
